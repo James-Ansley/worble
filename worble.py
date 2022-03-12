84a6db046b5cc14cbda297267b79adf90a8269ff
James Ansley <ansleyjpf@gmail.com>
2022-03-12 18:33:33 +1300

    added database to show past scores

--- worble.py
+++ worble.py
@@ -3,9 +3,12 @@ import os
 import random
 import string
 from collections import Counter
-from pathlib import PurePath
+from math import log10, floor
+from pathlib import Path
 
-DATA_ROOT = PurePath('data')
+import sqlite3
+
+DATA_ROOT = Path(__file__).parent.resolve() / 'data'
 NUM_GUESSES = 6
 GREEN_HIGHLIGHT = '\033[42m'
 YELLOW_HIGHLIGHT = '\033[43m'
@@ -16,7 +19,6 @@ with open(DATA_ROOT / 'winning_words.txt', 'r') as f:
 
 with open(DATA_ROOT / 'words.txt', 'r') as f:
     all_words = f.read().splitlines()
-
 
 keyboard = {ch: 0 for ch in string.ascii_lowercase}
 
@@ -99,6 +101,18 @@ def get_guess(guess_num):
     return guess
 
 
+def guess_histogram():
+    CUR.execute('SELECT GUESSES FROM Scores')
+    data = [val[0] for val in CUR.fetchall()]
+    counts = {n: 0 for n in range(1, 7)} | Counter(data)
+    max_count = max(counts.values())
+    for guess, count in sorted(counts.items()):
+        if max_count > 17:
+            print(f'{guess}:{"*" * round(count / (max_count / 17))}')
+        else:
+            print(f'{guess}:{"*" * count}')
+
+
 def main():
     word = random.choice(winning_words)
     guess_num = 1
@@ -109,11 +123,25 @@ def main():
         past_guesses.append(format_guess(word, guess))
         print_game(word, guess, past_guesses, incorrect_chars(word, guess))
         if guess == word:
-            print(f'Congratulations! Word guessed in {guess_num} guesses')
-            return
+            break
         guess_num += 1
-    print(f'The word was: {word}\nBetter luck next time.')
+    if guess_num <= NUM_GUESSES:
+        CUR.execute(f'INSERT INTO Scores(GUESSES) VALUES ({guess_num})')
+        print(f'Congratulations! Word guessed in {guess_num} guesses')
+    else:
+        print(f'The word was: {word}\nBetter luck next time.')
+    guess_histogram()
 
 
 if __name__ == '__main__':
-    main()
+    CON = sqlite3.connect('worble.db')
+    CUR = CON.cursor()
+    CUR.execute('CREATE TABLE IF NOT EXISTS Scores('
+                'ID INTEGER PRIMARY KEY AUTOINCREMENT,'
+                'GUESSES INTEGER'
+                ')')
+    try:
+        main()
+    finally:
+        CON.commit()
+        CON.close()
